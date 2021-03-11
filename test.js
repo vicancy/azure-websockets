2edded390e4891c35221f3872a457a3b6d5a6bd7
Liangying Wei <lianwei@microsoft.com>
2021-03-11 16:43:04 +0800

    update to latest

--- test.js
+++ test.js
@@ -1,57 +1,65 @@
-const { WebPubSubServiceEndpoint, WebPubSubServiceRestClient, WebPubSubServer } = require('./webpubsub');
+const { WebPubSubServer } = require('./webpubsub')
+const dotenv = require('dotenv');
+const http = require('http');
 
-async function clientNegotiateSample() {
-    var se = new WebPubSubServiceEndpoint("conn");
-    var clientResponse = se.clientNegotiate('hub1');
-    console.log(clientResponse);
-}
+dotenv.config();
 
-async function restClientSample() {
-    var rest = new WebPubSubServiceRestClient("conn", "hub");
-    await rest.sendToAll("message");
-    await rest.sendToUser("vicancy");
-}
+const wpsserver = new WebPubSubServer(process.env.WPS_CONNECTION_STRING, 'chat');
 
-async function serverListenSample() {
-    var wpsserver = new WebPubSubServer("conn", 'chat',
-        {
-            // eventHandlerUrl: "/customUrl", // optional
-            onConnect: async connectRequest => {
-                // success with client joining group1
-                // await wpsserver.broadcast(connectRequest.context.connectionId);
-                console.log(connectRequest.context);
-                return {
-                    userId: "vicancy"
-                }; // or connectRequest.fail(); to 401 the request
-            },
-            onConnected: async connectedRequest => {
-                await wpsserver.sendToAll(connectedRequest.context.connectionId + " connected");
-            },
-            onUserEvent: async userRequest => {
-                return {
-                    body: "Hey " + userRequest.data,
-                };
-            },
-            onDisconnected: async disconnectRequest => {
-                console.log(disconnectRequest.context.userId + " disconnected");
-            }
-        });
-
-    const port = 3000;
-
-    const server = http.createServer(async (request, response) => {
-        if (await wpsserver.handleNodeRequest(request, response)) {
-            console.log(`Processed ${request.url}`);
+// sample of client negotiate
+console.log(wpsserver.endpoint.clientNegotiate('chat', {
+    userId: "vicancy",
+    claims: {
+      hey: ["w"],
+      role: ["webpubsub.group.join"],
+   }
+  }));
+  
+const serviceClient = wpsserver.createServiceClient();
+const handler = wpsserver.createCloudEventsHandler(
+  {
+    //path: "/customUrl", // optional
+    onConnect: async connectRequest => {
+      return {
+        userId: "vicancy"
+      };
+    },
+    onConnected: async connectedRequest => {
+      await serviceClient.sendToAll(connectedRequest.context.connectionId + " connected");
+    },
+    onUserEvent: async userRequest => {
+      console.log(`Received user request data: ${userRequest.payload.data}`);
+      if (userRequest.payload.data === 'abort') {
+        return {
+          error: {
+            detail: "aborted"
+          }
+        };
+      }
+      if (userRequest.payload.data === 'error') {
+        throw new Error("error from inside the event");
+      }
+      return {
+        payload: {
+          data: "Hey " + userRequest.payload.data,
+          dataType: userRequest.payload.dataType
         }
-        else {
-            console.log(`${request.url} for others to process`);
-            response.statusCode = 404;
-            response.end();
-        }
-    });
+      };
+    },
+    onDisconnected: async disconnectRequest => {
+      console.log(disconnectRequest.context.userId + " disconnected");
+    }
+  }
+);
 
-    server.listen(port, () => console.log(`Azure WebPubSub Upstream ready at http://localhost:${port}${wpsserver.eventHandlerUrl}`));
+const port = 3000;
 
-}
+const server = http.createServer(async (request, response) => {
+  if (!await handler.handleRequest(request, response)) {
+    console.log(`${request.url} for others to process`);
+    response.statusCode = 404;
+    response.end();
+  }
+});
 
-serverListenSample();
+server.listen(port, () => console.log(`Azure WebPubSub Upstream ready at http://localhost:${port}${handler.path}`));
